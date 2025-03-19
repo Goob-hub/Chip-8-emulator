6936e7713447a54aee3677f6569a253b67701980
Goob-tech <solizg02@gmail.com>
2025-03-19 11:52:32 -0500

    updated main function and added config setup
    Added function that will handle setting up the config for the chip 8
    
    updated main function to handle arguments and inputs from console

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <process.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include "chip8.h"
 #include "../SDL3/SDL.h"
 
@@ -36,31 +37,43 @@ bool init_sdl(sdl_t *sdl, const config_t config) {
     return true;
 }
 
-void final_cleanup(window) {
-    SDL_DestroyWindow(window);
+bool setup_config(config_t *config, const int argc, const int **argv) {
+    //Set default values of chip-8 emulator
+    *config = (config_t) {
+        .window_width = 64,
+        .window_height = 32
+    };
+}
+
+void final_cleanup(const sdl_t sdl) {
+    SDL_DestroyWindow(sdl.window);
     SDL_Quit();
 }
 
-int main() {
+int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
     
-    sdl_t sdl = {NULL};
-    config_t config = {64, 32};
+    sdl_t sdl = {0};
+    config_t config = {0};
+    bool done = false;
+
+    init_config(config, argc, argv);
 
     if(!init_sdl(&sdl, config)) {
-        return 1;
+        exit(1);
     }
 
-    // while (!done) {
-    //     SDL_Event event;
+    while (!done) {
+        SDL_Event event;
 
-    //     while (SDL_PollEvent(&event)) {
-    //         if (event.type == SDL_EVENT_QUIT) {
-    //             done = true;
-    //         }
-    //     }
-    // }
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                done = true;
+            }
+        }
+    }
 
-    final_cleanup(sdl.window);
-
-    return 0;
+    final_cleanup(sdl);
+    exit(0);
 }
